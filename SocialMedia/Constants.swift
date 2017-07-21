f5b0ede0014cafd8a18b9fd5cd5160ffa35aac69
skpyahoo <sagarpahlajani@gmail.com>
2017-07-21 21:13:41 +0530

    Sign in design for custom view classes complete

---
+++ SocialMedia/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  SocialMedia
+//
+//  Created by Sagar Pahlajani on 21/07/17.
+//  Copyright © 2017 Sagar Pahlajani. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0

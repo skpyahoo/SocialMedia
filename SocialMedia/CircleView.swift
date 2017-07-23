f4826c91e8d6e79b40d657559c19fdac4cf3c5a9
skpyahoo <sagarpahlajani@gmail.com>
2017-07-23 20:18:03 +0530

    Done Layout of new Feed VC

--- SocialMedia/CircleView.swift
+++ SocialMedia/CircleView.swift
@@ -1,24 +1,32 @@
 //
-//  FancyView.swift
+//  CircleView.swift
 //  SocialMedia
 //
-//  Created by Sagar Pahlajani on 21/07/17.
+//  Created by Sagar Pahlajani on 23/07/17.
 //  Copyright © 2017 Sagar Pahlajani. All rights reserved.
 //
 
 import UIKit
 
-class FancyView: UIView {
+class CircleView: UIImageView {
 
     override func awakeFromNib() {
         
         super.awakeFromNib()
-        
         layer.shadowColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.6).cgColor
         layer.shadowOpacity = 0.8
         layer.shadowRadius = 5.0
         layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-        layer.cornerRadius = 2.0
+        
+        
     }
+    
+    override func draw(_ rect: CGRect) {
+        
+        super.draw(rect)
+        layer.cornerRadius = self.frame.width / 2
+
+    }
+    
 
 }
